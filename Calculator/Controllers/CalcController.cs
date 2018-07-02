74b5422024a1b3e3bd8303e5b0c3dabfb35ab4c9
Dima Kuzmenkov <kuzmenkovdmitrii@yandex.ru>
2018-07-02 16:51:05 +0300

    Added HTML page and litle programm logic

---
+++ Calculator/Controllers/CalcController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class CalcController : Controller
+    {
+        private double a = 0;
+
+        // GET: Calc
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public void PutA(double a)
+        {
+            this.a = a;
+        }
+
+        [HttpPost]
+        public double Sum(double b)
+        {
+            return a + b;
+        }
+
+        [HttpPost]
+        public double Subtraction(double b) //вычитание
+        {
+            return a - b;
+        }
+
+        [HttpPost]
+        public double Multiplication(double b)
+        {
+            return a * b;
+        }
+
+        [HttpPost]
+        public double Division(double b)
+        {
+            return a / b;
+        }
+
+        [HttpPost]
+        public double SqrtX(double b)
+        {
+            return Math.Pow(a, 1 / b);
+        }
+
+        [HttpPost]
+        public double DegreeY(double b)
+        {
+            return Math.Pow(a, b);
+        }
+
+        [HttpPost]
+        public double Sqrt()
+        {
+            return Math.Sqrt(a);
+        }
+
+        [HttpPost]
+        public double Square()
+        {
+            return Math.Pow(a, 2.0);
+        }
+    }
+}
---
+++ Calculator/Models/CalcModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public enum Action { Sum, Subtraction, Multiplication, Division, SqrtX, DegreeY, Sqrt, Square };
+
+namespace Calculator.Models
+{
+    public class CalcModel
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public Action Operation { get; set; }
+    }
+}
